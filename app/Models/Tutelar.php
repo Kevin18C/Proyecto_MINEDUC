0fc0106166493bd9c13728f7c06ed69ba0b36815
Carlos Martinez <cmartineza14@miumg.edu.gt>
2024-04-18 22:34:55 -0600

    Creacion de carpetas factories

--- app/Models/Tutelar.php
+++ app/Models/Tutelar.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Titular extends Model
+class Tutelar extends Model
 {
     use HasFactory;
 }
